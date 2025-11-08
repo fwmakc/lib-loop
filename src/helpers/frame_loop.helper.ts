a3ec11b43c6a9238993d11b46e85e4ca93e515b7
fwmakc <fwmakcpro@yandex.ru>
2025-11-08 21:48:17 +0300

    update to version 0.1.1

--- src/helpers/frame_loop.helper.ts
+++ src/helpers/frame_loop.helper.ts
@@ -1,19 +1,15 @@
 import { frameRate } from './frame_rate.helper';
-import { wait } from './wait.helper';
 
 /**
  * Starts loop that calls specified callback with updated time and ticks per second.
  *
  * @param {function} callback - Asynchronous function that returns boolean value.
  *                              Loop will executing while function returns true.
- * @param {number} [milliseconds=0] - Amount of time to wait between each iteration.
- *                                    If set to 0, no delay will be applied.
  * @param {any} [context=null] - Context `this` in which callback should be executed.
  * @returns {Promise<void>} Promise that resolves when loop is exited.
  */
 export async function frameLoop(
   callback: (deltaTime: number) => Promise<boolean> | boolean,
-  milliseconds = 0,
   context: any = null,
 ): Promise<void> {
   const frameRateMs = await frameRate();
@@ -32,10 +28,6 @@ export async function frameLoop(
       } catch (error) {
         console.error('An error occurred in loop:', error);
         infinite = false;
-      }
-
-      if (infinite && milliseconds) {
-        await wait(milliseconds);
       }
 
       if (infinite) {
